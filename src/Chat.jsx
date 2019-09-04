24951d1665035b54d2f10177f3751179825c661e
Maciej-Dusza <maciej-dusza@wp.pl>
2019-09-04 13:07:04 +0200

    Chat fetch comments from beckend

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -7,6 +7,12 @@ const ChatStyled=styled.div`
     padding: 10px;
 `;
 
+const ChatHistory=styled.div`
+    width: 300px;
+    background-color: white;
+    padding: 10px;
+`;
+
 export class Chat extends React.Component{
     
     constructor(){
@@ -16,18 +22,60 @@ export class Chat extends React.Component{
             commentsHistory:[]
         };
         this.getComment=this.getComment.bind(this);
+        this.commentsList=this.commentsList.bind(this);
+        this.sendComment=this.sendComment.bind(this);
     }
 
     componentDidMount() {
-        fetch("https://connect-comments-api.herokuapp.com/api/comments")
-          .then(comments => comments.json())
-          .then(json => console.log(json)); 
-      };
+       this.refreshComments()
+    };
     
-   
+
+    refreshComments(){
+        fetch("https://connect-comments-api.herokuapp.com/api/comments")
+            .then(comments => comments.json())
+            .then(json => this.setState({commentsHistory: json})); 
+            };
+
+
     getComment(){
         this.setState({comment: event.target.value})
-    }
+    };
+
+    commentsList(comments) {
+        return (
+          <div>
+            {comments.map((item) => (
+              <div  key={item._id}>
+                <div>{`${item.user}: ${item.text}`}</div>
+              </div>
+            ))}
+          </div>
+        );
+      }
+
+    sendComment() {
+        const apiUrl ="https://connect-comments-api.herokuapp.com/api/comments";
+        const options = {
+            method: "POST",
+            body: JSON.stringify({
+                user: this.props.activeUser,
+                text: this.state.comment
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              }
+        };
+
+        fetch(apiUrl, options)
+            .then(comments => comments.json())
+            .then(result => {
+                 this.setState({comment:""});
+                 this.refreshComments();   
+                }
+            )
+    };
 
     render(){
         return(
@@ -35,9 +83,13 @@ export class Chat extends React.Component{
             <div>Chat {this.props.activeUser}</div>
             <div>
                 <textarea value={this.state.comment} onChange={this.getComment}/>
+                <button onClick={this.sendComment}>Send</button>
             </div>
             <div>
                 {this.state.comment}
+                <ChatHistory>
+                    {this.commentsList(this.state.commentsHistory)}
+                </ChatHistory>
             </div>
         </ChatStyled>
         );
---
+++ src/Timer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const React = require('react')
+const ms = require('pretty-ms')
+
+export class Timer extends React.Component{
+
+    render(){
+
+        
+        return
+    }
+}
